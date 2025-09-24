36be9e778349e0d58046fd4b9073f6cd22dc012f
SiamakSm <siamak036@gmail.com>
2025-09-24 19:01:25 +0200

    Inheritance, Polymorphism, Encapsulation, Factory Method

--- main.js
+++ main.js
@@ -3,38 +3,129 @@
 import * as util from "./utils.js";
 
 const lib = new util.Library();
+lib.loadStorage();
 
-const idI = document.getElementById("id");
-const titleI = document.getElementById("title");
-const authorI = document.getElementById("author");
-const copiesI = document.getElementById("copies");
+const typeInput = document.getElementById("typeInput");
+const titleInput = document.getElementById("titleInput");
+const authorInput = document.getElementById("authorInput");
+const copiesInput = document.getElementById("copiesInput");
+const filesizeInput = document.getElementById("filesizeInput");
 
-const create = document.getElementById("create");
+const createBtn = document.getElementById("createBtn");
+const showBtn = document.getElementById("showBtn");
 const result = document.getElementById("result");
-const show = document.getElementById("show");
+const filesizeLabel = document.getElementById("filesize-label");
+const copiesLabel = document.getElementById("copies-label");
+
+const borrowBtn = document.getElementById("borrowBtn");
+const returnBtn = document.getElementById("returnBtn");
+const borrowIdInput = document.getElementById("borrowIdInput");
+const returnIdInput = document.getElementById("returnIdInput");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
-create.addEventListener("click", () => {
+typeInput.addEventListener("change", (e) => {
+  const value = e.target.value;
+  copiesLabel.classList.toggle("hidden", value !== "book");
+  filesizeLabel.classList.toggle("hidden", value !== "ebook");
+});
 
-  const id = idI.value;
-  const title = titleI.value;
-  const author = authorI.value;
-  const copies = parseInt(copiesI.value, 10);
 
-  if (!id || !title || !author || Number.isNaN(copies)) {
-    result.innerHTML = "<br>⚠️ Please fill ID, Title, Author, and Copies (number).";
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+createBtn.addEventListener("click", () => {
+
+  const type = typeInput.value;
+  const title = titleInput.value.trim();
+  const author = authorInput.value.trim();
+
+  if (!title || !author) {
+    result.textContent = "⚠️ Please fill Title and Author.";
     return;
   };
 
-  const book = new util.Book(id, title, author, copies);
-  lib.addBook(book);
+  const copiesOrSize = parseInt((type === "book" ? copiesInput.value : filesizeInput.value), 10);
+  if (Number.isNaN(copiesOrSize) || copiesOrSize < 0) {
+    result.textContent = (type === "book") ? "⚠️ Copies must be a non-negative number." : "⚠️ File size (MB) must be a positive number.";
+    return;
+  };
+
+  const book = bookFactory(null, type, title, author, copiesOrSize);
+  const res = lib.addBook(book);
+  result.textContent = res?.msg ?? "Added.";
+
+
+  titleInput.value = "";
+  authorInput.value = "";
+  copiesInput.value = "";
+  filesizeInput.value = "";
 
 });
 
-show.addEventListener("click", () => {
-  const lines = lib.listBooks();
-  result.innerHTML = `<br><br><br><strong>Books:</strong><br>${lines.join("<br>")}`;
-})
 
+showBtn.addEventListener("click", () => {
+  const lines = lib.listBooks();
+  let ul = document.createElement("ul");
+  lines.forEach(function (line) {
+    let li = document.createElement("li");
+    if (line.type === "book") {
+      li.textContent = `${line.id}. The Book ${line.title} , written by ${line.author},  has ${line.copies} copies !`;
+    } else {
+      li.textContent = `${line.id}. The Ebook ${line.title} , recorded by ${line.author},  has ${line.fileSizeMB} MB size !`;
+    };
+    ul.appendChild(li);
+  });
+  result.innerHTML = "";
+  result.appendChild(ul);
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+borrowBtn.addEventListener("click", function () {
+  result.innerHTML = "";
+  const borrowId = parseInt(borrowIdInput.value, 10);
+
+  if (Number.isNaN(borrowId)) {
+    result.textContent = "⚠️ Enter a valid numeric ID.";
+    return;
+  };
+
+  const res = lib.borrowById(borrowId);
+  result.innerHTML = res.msg;
+
+});
+
+
+
+returnBtn.addEventListener("click", function () {
+  result.innerHTML = "";
+  const returnId = parseInt(returnIdInput.value, 10);
+
+  if (Number.isNaN(returnId)) {
+    result.textContent = "⚠️ Enter a valid numeric ID.";
+    return;
+  };
+
+  const res = lib.returnById(returnId);
+  result.innerHTML = res.msg;
+
+});
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+function bookFactory(id, type, title, author, copiesOrSize) {
+  if (type === "book") return new util.Book(id, title, author, copiesOrSize);
+  if (type === "ebook") return new util.EBook(id, title, author, copiesOrSize);
+  throw new Error("Unknown type: " + type);
+};
 
 
--- utils.js
+++ utils.js
@@ -1,89 +1,146 @@
 // utils.js
 
-export class Book {
-  constructor(id, title, author, copies) {
-    this.id = id;
+class LibraryItem {
+  constructor(id, title, author) {
+    if (new.target === LibraryItem) {
+      throw new Error("Cannot instantiate abstract class LibraryItem directly");
+    };
+    this.id = id ?? null;
     this.title = title;
     this.author = author;
+  };
+
+  borrowOne() {
+    throw new Error("borrowOne() must be implemented in subclass");
+  };
+
+  returnOne() {
+    throw new Error("returnOne() must be implemented in subclass");
+  };
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+export class Book extends LibraryItem {
+  constructor(id, title, author, copies) {
+    super(id, title, author);
+    this.type = "book";
     this.copies = copies;
   };
 
-  getInfo() {
-    return `${this.title} - ${this.author} - x${this.copies}`;
+  borrowOne() {
+    if (this.copies > 0) { this.copies -= 1; return true; }
+    return false;
   };
 
-  borrowOne(title) {
-    return this.title;
+  returnOne() {
+    this.copies += 1;
+    return this.copies;
   };
+};
 
-  returnOne(title) {
-    return this.title;
-  };
 
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-
-export class EBook extends Book {
+export class EBook extends LibraryItem {
   constructor(id, title, author, fileSizeMB) {
-    super(id, title, author, 1);
+    super(id, title, author);
+    this.type = "ebook";
     this.fileSizeMB = fileSizeMB;
   };
 
-  overrideInfo() {
-    return this.title;
+  borrowOne() {
+    return true;
   };
-}
+
+  returnOne() {
+    return Infinity;
+  };
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
 
 export class Library {
-  constructor() {
-    this.items = [];
+  #items = [];
+  #nextId = 1;
+
+  generateId() {
+    return this.#nextId++;
   };
 
   addBook(book) {
-    this.items.push(book);
+    if (!(book instanceof LibraryItem)) {
+      return { ok: false, msg: "Only LibraryItem instances can be added." };
+    };
+
+    if (!book.id) book.id = this.generateId();
+    this.#items.push(book);
     this.saveStorage();
+    return { ok: true, msg: `Added [${book.id}] ${book.title}` };
   };
 
   saveStorage() {
-    localStorage.setItem("library", JSON.stringify(this.items));
+    localStorage.setItem("library", JSON.stringify({
+      items: this.#items,
+      nextId: this.#nextId
+    }));
+    return { ok: true, msg: `Saved to Storage!` };
   };
 
   loadStorage() {
     const raw = localStorage.getItem("library");
     if (raw) {
       const arr = JSON.parse(raw);
-      this.items = arr.map(obj => new Book(obj.id, obj.title, obj.author, obj.copies))
+      this.#nextId = arr.nextId || 1;
+      this.#items = arr.items.map(obj => {
+        return (obj.type === "ebook")
+          ? new EBook(obj.id, obj.title, obj.author, obj.fileSizeMB)
+          : new Book(obj.id, obj.title, obj.author, obj.copies)
+      });
     };
   };
 
   listBooks() {
     this.loadStorage();
-    return this.items.map(b => b.getInfo());
+    return this.#items;
   };
 
-  findByTitle(title) {
-    return this.items[title];
+  findById(id) {
+    this.loadStorage();
+    return this.#items.find(b => b.id === (Number(id)));
   };
 
   borrowById(id) {
-    return this.items[id];
+    const res = this.findById(id);
+    if (!res) return { ok: false, msg: `Not found with : "${id}"` };
+
+    const ok = res.borrowOne();
+    if (!ok) {
+      return { ok: false, msg: `No more copies for "${res.title}"` };
+    }
+
+    if (res.type === "book") this.saveStorage();
+
+    return {
+      ok: true,
+      msg: res.type === "book"
+        ? `Borrowed (Book): ${res.title} — left: ${res.copies}`
+        : `Borrowed (EBook): ${res.title} — unlimited`
+    };
   };
 
-  returnByTitle(title) {
-    return this.items[id];
-  };
+  returnById(id) {
+    const res = this.findById(id);
+    if (!res) return { ok: false, msg: `Not found with : "${id}"` };
 
-  totalPhysical() {
-    //let total = this.items.filter(p => p.id !== id);
-    return total;
+    const val = res.returnOne();
+    if (res.type === "book") {
+      this.saveStorage();
+      return { ok: true, msg: `Returned (Book): ${res.title} — copies: ${val}` };
+    }
+    return { ok: false, msg: `Return not needed for Ebooks.` };
   };
-
-}
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
